b9587498a7ebb77975129c08bce6de8293aa353f
KyykMaks <ripperkyyk@gmail.com>
2023-11-23 19:14:13 +0200

    update task 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,7 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const itemFrame = document.querySelector(".iframe");
+const player = new Player('your-video-player-id');
+
+const onPlay
