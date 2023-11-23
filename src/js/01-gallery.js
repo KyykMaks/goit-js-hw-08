e45b96102c4b99540e068d0767be3e2e30d517e9
KyykMaks <ripperkyyk@gmail.com>
2023-11-23 18:36:33 +0200

    update hwb8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galerryEl = document.querySelector(".gallery");
+
+galerryEl.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+
+function createMarkup(array) {  
+  return array  
+    .map(  
+      ({ preview, original, description }) => { 
+        return `  
+          <li class="gallery__item">  
+            <a class="gallery__link" href="${original}">  
+              <img class="gallery__image" src="${preview}" alt="${description}"/>  
+            </a>  
+          </li>  
+        `; 
+      }  
+    )  
+    .join(" ");  
+}
+
+  new SimpleLightbox(".gallery__item a", {
+    captions: true,
+    captionsDay: 250,
+  })
